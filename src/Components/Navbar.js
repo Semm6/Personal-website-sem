ba3a1ce2344ee46d7af36a954902cd5667e814f1
Semm6 <sem.dewilde6@gmail.com>
2021-12-30 21:29:10 +0100

    Added skills and projects layout

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,10 +8,10 @@ import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-// import { makeStyles } from "@material-ui/core/styles";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import "../Style/navbar.css";
+import pf from "../Images/pf.jpeg";
 
 const pages = [
   <Button variant="inherit" href="/about">
@@ -43,71 +43,65 @@ const Navbar = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <div className="centernav">
-      <AppBar position="fixed">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 0 }}>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="SDW" src="/static/images/avatar/3.jpg" />
-              </IconButton>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 100 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="SDW" src={pf} />
+            </IconButton>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
               {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {page}
-                </Button>
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
               ))}
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </div>
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
 export default Navbar;
--- src/Pages/Skills.js
+++ src/Pages/Skills.js
@@ -1,8 +1,69 @@
+import "../Style/skills.css";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(3),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.primary.main,
+}));
+
 function Skills() {
   return (
-    <div className="App">
-      <h1>Skills</h1>
-    </div>
+    <html>
+      <body>
+        <div>
+          <h1>Skills 🎯</h1>
+          <div className="info">
+            <h2>Web dev</h2>
+            <hr className="line" />
+            <div className="grid">
+              <Grid item xs={16}>
+                <Item>HTML CSS</Item>
+              </Grid>
+            </div>
+            <div className="grid">
+              <Grid item xs={16}>
+                <Item>JavaScript</Item>
+              </Grid>
+            </div>
+            <div className="grid">
+              <Grid item xs={16}>
+                <Item>React</Item>
+              </Grid>
+            </div>
+          </div>
+          <div className="info">
+            <h2>App dev</h2>
+            <hr className="line" />
+            <p></p>
+          </div>
+          <div className="info">
+            <h2>Database</h2>
+            <hr className="line" />
+            <p></p>
+          </div>
+          <div className="info">
+            <h2>Version control</h2>
+            <hr className="line" />
+            <p></p>
+          </div>
+          <div className="info">
+            <h2>General skills</h2>
+            <hr className="line" />
+            <p></p>
+          </div>
+          <div className="info">
+            <h2>Languages</h2>
+            <hr className="line" />
+            <p></p>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
 
